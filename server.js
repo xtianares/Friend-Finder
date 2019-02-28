139813e33112b88e2f5c806fd774693cab39a0e8
xtianares <xtianares@yahoo.com>
2019-02-27 23:00:58 -0500

    added empty routes scaffolding

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-// Dependencies
+// dependencies
 let express = require("express"),
     path = require("path");
 
-// Setup the Express App
+// setup the Express App
 let app = express(),
     PORT = process.env.PORT || 3000;
 
-// Setup the Express app to handle data
+// setup the Express app to handle data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
 
-// Starts the server to begin listening
+// starts the server to begin listening
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
 });
+
+// app router
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
