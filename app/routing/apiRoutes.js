1ced0e2fe9fcb7a394d5d964111b3ce305b0f1e1
xtianares <xtianares@yahoo.com>
2019-02-28 23:55:39 -0500

    fixed path to pul]blic folder, added api route

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,3 +1,12 @@
+// importing friends data
+var friends = require('../data/friends.js');
+
+// setting all API routes here
 module.exports = function(app) {
+
+	// for a raw list of friends in JSON format
+	app.get('/api/friends', function(req, res) {
+		res.json(friends);
+	});
 
 };
--- server.js
+++ server.js
@@ -2,20 +2,20 @@
 let express = require("express"),
     path = require("path");
 
-// setup the Express App
+// setup the express app
 let app = express(),
     PORT = process.env.PORT || 3000;
 
-// setup the Express app to handle data
+// setup the express app to handle data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "/public")));
-
-// starts the server to begin listening
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-});
+app.use(express.static(path.join(__dirname, './app/public'))); // this is the server's root folder
 
 // app router
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
+
+// starts the server and begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
