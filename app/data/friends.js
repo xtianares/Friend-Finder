f0253a562766164d40b7de6eb4b095268140a564
xtianares <xtianares@yahoo.com>
2019-03-03 17:04:51 -0500

    working friends score comparison function, need function to determine match

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,6 +3,16 @@ var friends = [
         "name":"Ahmed",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    },
+    {
+        "name":"Eddie",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[3, 1, 5, 2, 5, 3, 2, 5, 4, 5]
+    },
+    {
+        "name":"Test",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -16,6 +16,28 @@ module.exports = function(app) {
             userName = userInput.name,
             userScores = userInput.scores;
 
+        // check for a friend match
+        let matchArr = [], // this will store all the friend's score comparison, lowest score is the match
+            scoreDiff = 0; // this will store the current friend's total score
+
+        friends.forEach(function(item, index) {
+            // console.log(item.scores);
+            // console.log(index);
+
+            let scores = item.scores; // scores of the current potential fiend
+
+            scores.forEach(function(item, index) {
+                let questionDiff = Math.abs(item - userScores[index]); // diff for current question
+                scoreDiff += questionDiff; // adding the current question's diff to the total diff
+                console.log(scoreDiff);
+            });
+
+            matchArr.push(scoreDiff);
+            scoreDiff = 0;
+            console.log(matchArr);
+        });
+
+        // need to push new person after match is found otherwise person will match self
         friends.push(userInput);
 
         // need to update this with the match
